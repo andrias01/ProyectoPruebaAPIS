3f37c4a5642e41d13f5965b23d1d8aea6202211e
Andres Velez <andresjobpk@gmail.com>
2025-10-11 22:36:07 -0500

    El buil creado anteriormente presento error:
    Cuando se iniciaba el buil se presento un problema con el text, como iniciaba vault primero no y al mismo tiempo el text se intentaba conectar a la base de datos no la encontaba porque vault no terminaba de iniciar entonces se genetaba el error

--- src/test/java/co/edu/uco/easy/victusresidencias/victus_api/VictusRApplicationTests.java
+++ src/test/java/co/edu/uco/easy/victusresidencias/victus_api/VictusRApplicationTests.java
@@ -2,8 +2,10 @@ package co.edu.uco.easy.victusresidencias.victus_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VictusRApplicationTests {
 
 	/**
